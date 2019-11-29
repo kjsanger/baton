ac93bca048e5c60636087a8851b6958708bae8d9
Keith James <dev@deoxybyte.co.uk>
2019-11-29 10:43:06 +0000

    Update ccpp.yml

--- scripts/setup_conda.sh
+++ scripts/setup_conda.sh
@@ -12,20 +12,3 @@ conda activate github
 conda install -y libjansson-dev
 conda install -y irods-dev
 conda install -y irods-icommands
-
-mkdir -p $HOME/.irods
-cat <<EOF > $HOME/.irods/irods_environment.json
-{
-    "irods_host": "irods",
-    "irods_port": 1247,
-    "irods_user_name": "irods",
-    "irods_zone_name": "testZone",
-    "irods_home": "/testZone/home/irods",
-    "irods_plugins_home": "$CONDA_INSTALL_DIR/envs/github/lib/irods/plugins/",
-    "irods_default_resource": "testResc"
-}
-EOF
-
-echo "irods" | script -q -c "iinit" /dev/null
-ienv
-ils
---
+++ scripts/setup_irods.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -ex
+
+mkdir -p $HOME/.irods
+cat <<EOF > $HOME/.irods/irods_environment.json
+{
+    "irods_host": "irods-service",
+    "irods_port": 1247,
+    "irods_user_name": "irods",
+    "irods_zone_name": "testZone",
+    "irods_home": "/testZone/home/irods",
+    "irods_plugins_home": "$CONDA_INSTALL_DIR/envs/github/lib/irods/plugins/",
+    "irods_default_resource": "testResc"
+}
+EOF
+
+echo "irods" | script -q -c "iinit" /dev/null
+ienv
+ils

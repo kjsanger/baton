72884486a493ea991e49da2e0223d0daf3a08ef2
Keith James <dev@deoxybyte.co.uk>
2019-12-02 15:24:20 +0000

    Update ccpp.yml

--- scripts/build_baton.sh
+++ scripts/build_baton.sh
@@ -10,6 +10,8 @@ CONDA_ENV="$CONDA_INSTALL_DIR/envs/github"
 CPPFLAGS="-I$CONDA_ENV/include -I$CONDA_ENV/include/irods"
 LDFLAGS="-L$CONDA_ENV/lib -L$CONDA_ENV/lib/irods/externals"
 
+autoreconf -fi
+
 ./configure --with-test-resource=testResc \
             CPPFLAGS="$CPPFLAGS" LDFLAGS="$LDFLAGS"
 
